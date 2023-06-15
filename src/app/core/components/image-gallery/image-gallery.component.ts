d92ac37e885fd3e5e7821b5fe6658644227fb8f4
lior daniel <liorqm@gmail.com>
2023-06-15 16:15:35 +0300

    rggr

--- src/app/core/components/image-gallery/image-gallery.component.ts
+++ src/app/core/components/image-gallery/image-gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-image-gallery',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./image-gallery.component.css']
 })
 export class ImageGalleryComponent {
+  constructor(private router:Router){}
   imageItems = [
     { imageUrl: 'https://img.yad2.co.il/Pic/202303/18/2_1/o/y2_1_04417_20230318123544.jpeg' },
     { imageUrl: 'https://img.yad2.co.il/Pic/202303/18/2_1/o/y2_2_07922_20230318123553.jpeg' },
@@ -17,5 +19,9 @@ export class ImageGalleryComponent {
   ];
   stopProp(event:Event){
    event.stopPropagation()
+  }
+  navigateToImageGallery(event:Event){
+    this.stopProp(event)
+    this.router.navigate(['/realestate'])
   }
 }
