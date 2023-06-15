49fe8e690e35dbcaf20cfee16f97a7b3cc14099e
lior daniel <liorqm@gmail.com>
2023-06-15 15:46:01 +0300

    grrg

---
+++ src/app/core/components/image-gallery/image-gallery.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-gallery',
+  templateUrl: './image-gallery.component.html',
+  styleUrls: ['./image-gallery.component.css']
+})
+export class ImageGalleryComponent {
+  imageItems = [
+    { imageUrl: 'https://img.yad2.co.il/Pic/202303/18/2_1/o/y2_1_04417_20230318123544.jpeg' },
+    { imageUrl: 'https://img.yad2.co.il/Pic/202303/18/2_1/o/y2_2_07922_20230318123553.jpeg' },
+    { imageUrl: 'https://img.yad2.co.il/Pic/202303/18/2_1/o/y2_3_05484_20230318123616.jpeg' },
+    { imageUrl: 'https://img.yad2.co.il/Pic/202303/18/2_1/o/y2_4_08848_20230318123628.jpeg' },
+    { imageUrl: 'https://img.yad2.co.il/Pic/202303/18/2_1/o/y2_5_08773_20230318123918.jpeg' },
+    { imageUrl: 'https://img.yad2.co.il/Pic/202303/18/2_1/o/y2_6_02282_20230318130342.jpeg' },
+    { imageUrl: 'https://img.yad2.co.il/Pic/202303/18/2_1/o/y2_7_03870_20230318130413.jpeg' }
+  ];
+  stopProp(event:Event){
+   event.stopPropagation()
+  }
+}
