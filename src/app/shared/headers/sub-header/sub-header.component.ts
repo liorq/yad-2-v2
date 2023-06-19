4465a921ac8e7a51b86ee42cc3e0c2c4101d9a06
lior daniel <liorqm@gmail.com>
2023-06-19 14:08:20 +0300

    rggr

--- src/app/shared/headers/sub-header/sub-header.component.ts
+++ src/app/shared/headers/sub-header/sub-header.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./sub-header.component.css']
 })
 export class SubHeaderComponent {
+  buttons = [
+    { containerClass: 'doronContainer', text: 'דורון-העוזר האישי', iconClass: 'doron' },
+    { containerClass: 'yad1Container', text: 'יד1 דירות חדשות', iconClass: 'yad1' },
+    { containerClass: 'estimationContainer', text: 'הערכת שווי נכס', iconClass: 'estimation' },
+    { containerClass: 'hammerContainer', text: 'כונס נכסים', iconClass: 'hammer' }
+  ];
+
+  tabs = [
+    { label: 'מכירה', isActive: true },
+    { label: 'השכרה', isActive: false },
+    { label: 'דירות שותפים', isActive: false },
+    { label: 'נדל"ן מסחרי', isActive: false }
+  ];
 
 }
